aa5515cdef86b6e655aeda2de1a12b304c2e74f3
Rees <reesmorriscontact@gmail.com>
2020-12-11 18:32:42 +0000

    complete day1 part2

--- src/1/index.ts
+++ src/1/index.ts
@@ -4,6 +4,7 @@ interface findSumResult {
   success: boolean;
   num1: number;
   num2: number;
+  num3: number;
 }
 const findSum = (arr: string[], sum: number): findSumResult => {
   // Convert all strings to numbers
@@ -18,11 +19,17 @@ const findSum = (arr: string[], sum: number): findSumResult => {
   // Loop through each number to find a pair
   for (let i = 0; i < newArr.length; i++)
     for (let j = 0; j < newArr.length; j++)
-      if (newArr[i] + newArr[j] === sum)
-        return { success: true, num1: newArr[i], num2: newArr[j] };
+      for (let k = 0; k < newArr.length; k++)
+        if (newArr[i] + newArr[j] + newArr[k] === sum)
+          return {
+            success: true,
+            num1: newArr[i],
+            num2: newArr[j],
+            num3: newArr[k],
+          };
 
   // No results
-  return { success: false, num1: 0, num2: 0 };
+  return { success: false, num1: 0, num2: 0, num3: 0 };
 };
 
 const setup = () => {
@@ -31,8 +38,8 @@ const setup = () => {
     .toString()
     .split('\n');
 
-  const { success, num1, num2 } = findSum(inputs, 2020);
-  if (success) return console.log(num1 * num2);
+  const { success, num1, num2, num3 } = findSum(inputs, 2020);
+  if (success) return console.log(num1 * num2 * num3);
   return console.log('could not find sum');
 };
 setup();
