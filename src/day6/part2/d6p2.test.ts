b3dbcb43d0b8aa12f334623429275971bb0a9e8e
Rees <reesmorriscontact@gmail.com>
2020-12-14 22:17:17 +0000

    setup files for day6 part2

---
+++ src/day6/part2/d6p2.test.ts
@@ -0,0 +1,28 @@
+import { totalSimilarAnswers } from '.';
+
+test('totalSimilarAnswers function works with example values', () => {
+  expect(totalSimilarAnswers(`abc`)).toBe(3);
+
+  expect(
+    totalSimilarAnswers(`
+    a
+    b
+    c`)
+  ).toBe(0);
+
+  expect(
+    totalSimilarAnswers(`
+    ab
+    ac`)
+  ).toBe(1);
+
+  expect(
+    totalSimilarAnswers(`
+  a
+  a
+  a
+  a`)
+  ).toBe(1);
+
+  expect(totalSimilarAnswers(`b`)).toBe(1);
+});
