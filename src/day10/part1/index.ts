b20b97d1cda486d34d983a2075caffcc741ca9b5
Rees <reesmorriscontact@gmail.com>
2020-12-16 00:44:01 +0000

    setup files for day10 part1

---
+++ src/day10/part1/index.ts
@@ -0,0 +1,7 @@
+import fs from 'fs';
+
+export const largestChainMultiplication = (input: string) => {};
+
+export const setup = () => {
+  const input: string = fs.readFileSync(`${__dirname}/../input.txt`).toString();
+};
